256fa3b6f2a211dbfdca19431f252e06fe8760f2
HornedCoder <kayush5u2018@gmail.com>
2026-06-12 11:26:34 +0530

    Create 153_Find_Min_In_Rotated_Sorted_Array.js

---
+++ 153_Find_Min_In_Rotated_Sorted_Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    var l = 0;
+    var r = nums.length-1;
+    while (l < r){
+        let mid = Math.floor((l+r)/2);
+
+        if (nums[mid] > nums[r]){
+            l = mid+1;
+        }
+        else{
+            r = mid;
+        }
+    }
+    return nums[l];
+}  
